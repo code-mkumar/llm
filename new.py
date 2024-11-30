338697348787dcd3402052e4a579e06cf16144ed
code-mkumar <codehunting2k24@gmail.com>
2024-11-30 15:52:17 +0530

    Update new.py

--- new.py
+++ new.py
@@ -86,7 +86,7 @@ CREATE TABLE IF NOT EXISTS book (
     subject_id TEXT NOT NULL,
     book_details TEXT NOT NULL,
     FOREIGN KEY (subject_id) REFERENCES subject(id) ON DELETE CASCADE ON UPDATE CASCADE
-    FOREIGN KEY (user_id) REFERENCES user_detail(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    
 );
 ''')
 
